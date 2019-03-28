c6803f760161912d3502492da735a98f37e67a05
Johan Karlsson <johan.karlsson1@itab.com>
2019-03-28 11:06:40 +0100

    Removed file, added property output

--- core/src/main/java/com/demo/Application.java
+++ core/src/main/java/com/demo/Application.java
@@ -1,11 +1,18 @@
 package com.demo;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.PropertySource;
 
 import com.demo.azure.configuration.DataLakeConfiguration;
 import com.demo.azure.configuration.ProvisioningConfiguration;
@@ -19,9 +26,7 @@ import com.demo.azure.configuration.SpringProperties;
 public class Application {
 
     public static void main(final String[] args) {
-        new SpringApplicationBuilder(Application.class)
-            .bannerMode(Banner.Mode.OFF)
-            .run(args);
+        new SpringApplicationBuilder(Application.class).bannerMode(Banner.Mode.OFF).run(args);
     }
 
     @Autowired
@@ -36,11 +41,22 @@ public class Application {
     @Autowired
     public SpringProperties config4;
 
+    @Autowired
+    public Environment environment;
+
     @PostConstruct
     public void post() {
         final DataLakeConfiguration asdf = config;
         final ProvisioningConfiguration qwerty = config2;
         final ProvisioningConfigurationWithConfigurationProperties foo = config3;
         final SpringProperties bar = config4;
+
+        for(final Iterator it = ((AbstractEnvironment) environment).getPropertySources().iterator(); it.hasNext(); ) {
+            final PropertySource propertySource = (PropertySource) it.next();
+            System.out.println(propertySource.getClass());
+            if (propertySource instanceof EnumerablePropertySource) {
+                Arrays.stream(((EnumerablePropertySource) propertySource).getPropertyNames()).forEach(System.out::println);
+            }
+        }
     }
 }
